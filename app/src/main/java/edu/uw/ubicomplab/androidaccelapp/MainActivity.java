fa6b1df8bd933618b2902561949e4ad340067ed6
atm15 <atm15@cs.washington.edu>
2018-04-18 14:07:06 -0700

    first commit

---
+++ app/src/main/java/edu/uw/ubicomplab/androidaccelapp/MainActivity.java
@@ -0,0 +1,294 @@
+package edu.uw.ubicomplab.androidaccelapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    // GLOBALS
+    // Accelerometer
+    private LineGraphSeries<DataPoint> timeAccelX = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> timeAccelY = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> timeAccelZ = new LineGraphSeries<>();
+
+    // Gyroscope
+    private LineGraphSeries<DataPoint> timeGyroX = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> timeGyroY = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> timeGyroZ = new LineGraphSeries<>();
+
+    // Graph
+    private GraphView graph;
+    private int graphXBounds = 50;
+    private int graphYBounds = 50;
+    private int graphColor[] = {Color.argb(255,244,170,50),
+            Color.argb(255, 60, 175, 240),
+            Color.argb(225, 50, 220, 100)};
+    private static final int MAX_DATA_POINTS_UI_IMU = 100; // Adjust to show more points on graph
+    public int accelGraphXTime = 0;
+    public int gyroGraphXTime = 0;
+
+    // UI elements
+    private TextView resultText;
+    private TextView gesture1CountText, gesture2CountText, gesture3CountText;
+
+    // Machine learning
+    private Model model;
+    private boolean isRecording;
+    private DescriptiveStatistics accelTime, accelX, accelY, accelZ;
+    private DescriptiveStatistics gyroTime, gyroX, gyroY, gyroZ;
+    private static final int GESTURE_DURATION_SECS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the UI elements
+        resultText = findViewById(R.id.resultText);
+        gesture1CountText = findViewById(R.id.gesture1TextView);
+        gesture2CountText = findViewById(R.id.gesture2TextView);
+        gesture3CountText = findViewById(R.id.gesture3TextView);
+
+        // Initialize the graphs
+        initializeFilteredGraph();
+
+        // Initialize data structures for gesture recording
+        accelTime = new DescriptiveStatistics();
+        accelX = new DescriptiveStatistics();
+        accelY = new DescriptiveStatistics();
+        accelZ = new DescriptiveStatistics();
+        gyroTime = new DescriptiveStatistics();
+        gyroX = new DescriptiveStatistics();
+        gyroY = new DescriptiveStatistics();
+        gyroZ = new DescriptiveStatistics();
+
+        // Initialize the model
+        model = new Model(this);
+
+        // Get the sensors
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        Sensor gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        if (sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            accelGraphXTime += 1;
+
+            // Get the data from the event
+            long timestamp = event.timestamp;
+            float ax = event.values[0];
+            float ay = event.values[1];
+            float az = event.values[2];
+
+            // Add the original data to the graph
+            DataPoint dataPointAccX = new DataPoint(accelGraphXTime, ax);
+            DataPoint dataPointAccY = new DataPoint(accelGraphXTime, ay);
+            DataPoint dataPointAccZ = new DataPoint(accelGraphXTime, az);
+            timeAccelX.appendData(dataPointAccX, true, MAX_DATA_POINTS_UI_IMU);
+            timeAccelY.appendData(dataPointAccY, true, MAX_DATA_POINTS_UI_IMU);
+            timeAccelZ.appendData(dataPointAccZ, true, MAX_DATA_POINTS_UI_IMU);
+
+            // Advance the graph
+            graph.getViewport().setMinX(accelGraphXTime-graphXBounds);
+            graph.getViewport().setMaxX(accelGraphXTime);
+
+            // Add to gesture recorder, if applicable
+            if (isRecording) {
+                accelTime.addValue(timestamp);
+                accelX.addValue(ax);
+                accelY.addValue(ay);
+                accelZ.addValue(az);
+            }
+        }
+        else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            gyroGraphXTime += 1;
+
+            // Get the data from the event
+            long timestamp = event.timestamp;
+            float gx = event.values[0];
+            float gy = event.values[1];
+            float gz = event.values[2];
+
+            // Add the original data to the graph
+            DataPoint dataPointGyroX = new DataPoint(gyroGraphXTime, gx);
+            DataPoint dataPointGyroY = new DataPoint(gyroGraphXTime, gy);
+            DataPoint dataPointGyroZ = new DataPoint(gyroGraphXTime, gz);
+            timeGyroX.appendData(dataPointGyroX, true, MAX_DATA_POINTS_UI_IMU);
+            timeGyroY.appendData(dataPointGyroY, true, MAX_DATA_POINTS_UI_IMU);
+            timeGyroZ.appendData(dataPointGyroZ, true, MAX_DATA_POINTS_UI_IMU);
+
+            // Save to file, if applicable
+            if (isRecording) {
+                gyroTime.addValue(timestamp);
+                gyroX.addValue(event.values[0]);
+                gyroY.addValue(event.values[1]);
+                gyroZ.addValue(event.values[2]);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    /**
+     * Records a gesture that is MAX_GESTURE_DURATION_SECS long
+     */
+    public void recordGesture(View v) {
+        final View v2 = v;
+
+        // Create the timer to start data collection
+        Timer startTimer = new Timer();
+        TimerTask startTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        accelTime.clear(); accelX.clear(); accelY.clear(); accelZ.clear();
+                        gyroTime.clear(); gyroX.clear(); gyroY.clear(); gyroZ.clear();
+                        isRecording = true;
+                        v2.setEnabled(false);
+                    }
+                });
+            }
+        };
+
+        // Figure out which button got pressed to determine label
+        final String label;
+        final boolean isTraining;
+        switch (v.getId()) {
+            case R.id.gesture1Button:
+                label = model.outputClasses[0];
+                isTraining = true;
+                break;
+            case R.id.gesture2Button:
+                label = model.outputClasses[1];
+                isTraining = true;
+                break;
+            case R.id.gesture3Button:
+                label = model.outputClasses[2];
+                isTraining = true;
+                break;
+            default:
+                label = "?";
+                isTraining = false;
+                break;
+        }
+
+        // Create the timer to stop data collection
+        Timer endTimer = new Timer();
+        TimerTask endTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Add the recent gesture to the train or test set
+                        isRecording = false;
+                        model.addSample(accelTime, accelX, accelY, accelZ,
+                                gyroTime, gyroX, gyroY, gyroZ, label, isTraining);
+
+                        // Predict if the recent sample is for testing
+                        if (!isTraining) {
+                            String result = model.test();
+                            resultText.setText("Result: "+result);
+                        }
+
+                        // Update number of samples shown
+                        updateTrainDataCount();
+                        v2.setEnabled(true);
+                    }
+                });
+            }
+        };
+
+        // Start the timers
+        startTimer.schedule(startTask, 0);
+        endTimer.schedule(endTask, GESTURE_DURATION_SECS*1000);
+    }
+
+    /**
+     * Trains the model as long as there is at least one sample per class
+     */
+    public void trainModel(View v) {
+        // Make sure there is training data for each gesture
+        for (int i=0; i<model.outputClasses.length; i++) {
+            int gestureCount = model.getNumTrainSamples(i);
+            if (gestureCount == 0) {
+                Toast.makeText(getApplicationContext(), "Need examples for gesture" + (i+1),
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        // Train
+        model.train();
+    }
+
+    /**
+     * Resets the training data of the model
+     */
+    public void clearModel(View v) {
+        model.resetTrainingData();
+        updateTrainDataCount();
+        resultText.setText("Result: ");
+    }
+
+    /**
+     * Initializes the graph that will show filtered data
+     */
+    public void initializeFilteredGraph() {
+        graph = findViewById(R.id.graph);
+        graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.NONE);
+        graph.setBackgroundColor(Color.TRANSPARENT);
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graph.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(graphXBounds);
+        graph.getViewport().setMinY(-graphYBounds);
+        graph.getViewport().setMaxY(graphYBounds);
+        timeAccelX.setColor(graphColor[0]);
+        timeAccelX.setThickness(10);
+        graph.addSeries(timeAccelX);
+        timeAccelY.setColor(graphColor[1]);
+        timeAccelY.setThickness(10);
+        graph.addSeries(timeAccelY);
+        timeAccelZ.setColor(graphColor[2]);
+        timeAccelZ.setThickness(10);
+        graph.addSeries(timeAccelZ);
+    }
+
+    public void updateTrainDataCount() {
+        gesture1CountText.setText("Num samples: "+model.getNumTrainSamples(0));
+        gesture2CountText.setText("Num samples: "+model.getNumTrainSamples(1));
+        gesture3CountText.setText("Num samples: "+model.getNumTrainSamples(2));
+    }
+}
---
+++ app/src/main/java/edu/uw/ubicomplab/androidaccelapp/Model.java
@@ -0,0 +1,289 @@
+package edu.uw.ubicomplab.androidaccelapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import weka.classifiers.Classifier;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+
+public class Model {
+    // Examples of how the .arff format:
+    // https://www.programcreek.com/2013/01/a-simple-machine-learning-example-in-java/
+    // https://www.cs.waikato.ac.nz/~ml/weka/arff.html
+    private Map<String, ArrayList<String[]>> trainingData;
+    private String[] testData;
+    public Map<String, String> featureNames;
+    private String trainDataFilepath = "trainData.arff";
+    private String testDataFilepath = "testData.arff";
+    private Classifier model;
+    private Context context;
+
+    // TODO optional: give your gestures more informative names
+    public String[] outputClasses = {"Gesture1", "Gesture2", "Gesture3"};
+
+    public Model(Context context) {
+        this.context = context;
+        resetTrainingData();
+
+        // Specify the features
+        featureNames = new TreeMap<>();
+        // TODO optional: create more features with more informative names
+        featureNames.put("Feature1", "numeric");
+        featureNames.put("Feature2", "numeric");
+        featureNames.put("Feature3", "numeric");
+    }
+
+    /**
+     * Add a sample to the training or testing set with the corresponding label
+     * @param atime: the time for the accelerometer data
+     * @param ax: the x-acceleration data
+     * @param ay: the y-acceleration data
+     * @param az: the z-acceleration data
+     * @param gtime: the time for the accelerometer data
+     * @param gx: the x-gyroscope data
+     * @param gy: the y-gyroscope data
+     * @param gz: the z-gyroscope data
+     * @param outputLabel: the label for the data
+     * @param isTraining: whether the sample should go into the train or test set
+     */
+    public void addSample(DescriptiveStatistics atime, DescriptiveStatistics ax, DescriptiveStatistics ay, DescriptiveStatistics az,
+                             DescriptiveStatistics gtime, DescriptiveStatistics gx, DescriptiveStatistics gy, DescriptiveStatistics gz,
+                             String outputLabel, boolean isTraining) {
+        Double[] data = new Double[featureNames.keySet().size()];
+
+        // Compute features
+        // TODO: replace these placeholders with real calculations involving ax, ay, etc.
+        data[0] = 0.0;
+        data[1] = 0.0;
+        data[2] = 4.0;
+
+        // Convert the feature vector to Strings
+        String[] stringData = new String[featureNames.keySet().size()];
+        for (int i=0; i<featureNames.keySet().size(); i++) {
+            stringData[i] = Double.toString(data[i]);
+        }
+
+        // Add to the list of feature samples as strings
+        if (isTraining) {
+            ArrayList<String[]> currentSamples = trainingData.get(outputLabel);
+            currentSamples.add(stringData);
+            trainingData.put(outputLabel, currentSamples);
+        }
+        else {
+            testData = stringData;
+        }
+    }
+
+    /**
+     * Clears all of the data for the model
+     */
+    public void resetTrainingData() {
+        // Create a blank list for each gesture
+        trainingData = new LinkedHashMap<>();
+        for (String s: outputClasses) {
+            trainingData.put(s, new ArrayList<String[]>());
+        }
+    }
+
+    /**
+     * Returns the number of training samples for the given class index
+     * @param index: the class index
+     * @return the number of samples for the given class index
+     */
+    public int getNumTrainSamples(int index) {
+        String className = outputClasses[index];
+        return trainingData.get(className).size();
+    }
+
+    /**
+     * Create an .arff file for the dataset
+     * @param isTraining: whether the data is training or testing data
+     */
+    private void createDataFile(boolean isTraining) {
+        PrintWriter writer;
+        // Setup the file writer depending on whether it is training or testing data
+        if (isTraining)
+            writer = createPrintWriter(trainDataFilepath);
+        else
+            writer = createPrintWriter(testDataFilepath);
+
+        // Name the dataset
+        writer.println("@relation gestures");
+        writer.println("");
+
+        // Define the features
+        for (String s: featureNames.keySet()) {
+            writer.println("@attribute "+s+" "+featureNames.get(s));
+        }
+
+        // Define the possible output classes
+        String outputOptions = "@attribute gestureName {";
+        for (String s: outputClasses) {
+            outputOptions += s+", ";
+        }
+        outputOptions = outputOptions.substring(0, outputOptions.length()-2);
+        outputOptions += "}";
+        writer.println(outputOptions);
+        writer.println("");
+
+        // Write the data
+        writer.println("@data");
+        if (isTraining) {
+            // Go through each category of possible outputs and save their samples
+            for (String s: outputClasses) {
+                ArrayList<String[]> gestureSamples = trainingData.get(s);
+                for (String[] sampleData: gestureSamples) {
+                    String sample = "";
+                    for (String x: sampleData) {
+                        sample += x+",";
+                    }
+                    sample += s;
+                    writer.println(sample);
+                }
+            }
+        }
+        else {
+            // Write the new sample with a blank label
+            String sample = "";
+            for (String x: testData) {
+                sample += x+",";
+            }
+            sample += "?";
+            writer.println(sample);
+        }
+        writer.close();
+    }
+
+    /**
+     * Trains a model for the training data
+     */
+    public void train() {
+        // Create the file for training
+        createDataFile(true);
+
+        // Read the file and specify the last index as the class
+        Instances trainInstances = createInstances(trainDataFilepath);
+        if (trainInstances == null) {
+            return;
+        }
+        trainInstances.setClassIndex(trainInstances.numAttributes()-1);
+
+        // Define the classifier
+        // TODO optional: try out different classifiers provided by Weka
+        model = new J48();
+        try {
+            model.buildClassifier(trainInstances);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    /**
+     * Returns the string label for the recently tested gesture
+     * @return the string label
+     */
+    public String test() {
+        // Create the file for testing
+        createDataFile(false);
+
+        // Read the file and specify the last index as the class
+        Instances testInstances = createInstances(testDataFilepath);
+        testInstances.setClassIndex(testInstances.numAttributes()-1);
+
+        // Predict
+        String classLabel = null;
+        try {
+            double classIndex = model.classifyInstance(testInstances.instance(0));
+            classLabel = testInstances.classAttribute().value((int) classIndex);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return classLabel;
+    }
+
+    /**
+     * Reads the .arff file and converts it into an Instances object
+     * @param filename the filepath for the .arff file
+     * @return a newly created Instances object
+     */
+    private Instances createInstances(String filename) {
+        // Check read permissions
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context,
+                    "Please give app permissions in Settings > Installed apps > AndroidAccelApp",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
+        // Read the file
+        File SDFile = android.os.Environment.getExternalStorageDirectory();
+        String fullFileName = SDFile.getAbsolutePath() + File.separator + filename;
+        BufferedReader dataReader;
+        try {
+            FileReader fileReader = new FileReader(fullFileName);
+            dataReader = new BufferedReader(fileReader);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // Create the training instance
+        Instances instances;
+        try {
+            instances = new Instances(dataReader);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(context,
+                    "Something is wrong with your .arff file!",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        return instances;
+    }
+
+    /**
+     * Creates the file at the location
+     * @param filename: the filename that appears at the root of external storage
+     * @return writer: the PrintWriter object to be used
+     */
+    public PrintWriter createPrintWriter(String filename) {
+        // Check permissions
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context,
+                    "Please give app permissions in Settings > Installed apps > AndroidAccelApp",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
+        // Create the file
+        File SDFile = android.os.Environment.getExternalStorageDirectory();
+        String fullFileName = SDFile.getAbsolutePath() + File.separator + filename;
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(fullFileName);
+        } catch(FileNotFoundException e) {
+            return null;
+        }
+        return writer;
+    }
+}
